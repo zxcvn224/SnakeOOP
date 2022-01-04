f334197cd539cec38044c738d6fe0736d0f8fad4
Opilane TTHK <opilane@tthk.ee>
2022-01-04 11:57:35 +0200

    SnakeOOP

---
+++ SnakeOOP/HorizontalLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeOOP
+{
+    class Horizon : Figure
+    {
+
+
+        public Horizon(int xLeft, int xRight, int y, char symb)
+        {
+            pointList = new List<Point>();
+
+            for (int i = xLeft; i <= xRight; i++)
+            {
+                Point p = new Point(i, y, symb);
+                pointList.Add(p);
+            }
+
+        }
+    }
+}
